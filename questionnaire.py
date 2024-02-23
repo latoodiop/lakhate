d5991cb1aa563116eac6984e9876164312ad0330
lakhate <abdoulakhatediop4@gmail.com>
2024-02-23 20:48:47 +0100

    une version plus ameliorer

--- questionnaire.py
+++ questionnaire.py
@@ -24,20 +24,32 @@ else:
 
 
 
-def poser_question(question, liste, choix_bonne_reponce):
+def poser_question(question):
     global score
-    print(question)
-    capitales = liste
-    for i in capitales:
-        print(i)
-    print("choisiser parmis les differents lettres la capitale")
-    reponse= input("entres la lettre correcte: ")
-    if reponse == choix_bonne_reponce:
+
+    titre_question=question[0]
+    liste=question[1]
+    choix_bonne_reponce=question[2]
+    
+    print(titre_question)
+    liste
+    compteur= len(liste)
+    for i in liste:
+        print(compteur-1, "-",i)
+
+    reponse= input("votre reponse (entre 1 et " + str(compteur+1) +"): ")
+    if reponse.lower() == choix_bonne_reponce:
         print("bonne reponse")
         score+=1
     else:
         print("c'est pas sa")
+
+
+
 score=0
-poser_question("quelle est la capitale de france?:", ["(a) marseille","(b) nice", "(c) paris", "(d) nantes"], "c")
-poser_question("quelle est la capitale d' italie?:", ["(a) rome","(b) inter", "(c) torrino", "(d) juventive"], "a")
+question1 = ("quelle est la capitale de france?:", (" marseille"," nice", " paris", " nantes"), "paris")
+question2 = ("quelle est la capitale d' italie?:", (" rome"," inter", " torrino", " juventive"), "rome")
+
+poser_question(question1)
+poser_question(question2)
 print("l'score final", score)
